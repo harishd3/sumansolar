757d1d126e221389dd58a548bfb84801cf95c7b7
harish dasari <harishdasari3@gmail.com>
2025-05-05 12:46:56 +0530

    solar v1.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,17 +8,30 @@ import { NotfoundComponent } from './notfound/notfound.component';
 import { FooterComponent } from './footer/footer.component';
 import { GalleryComponent } from './gallery/gallery.component';
 
+// const routes: Routes = [
+//   { path: "", redirectTo: "dashboard", pathMatch: "full" },  // <--- ADD THIS
+//   { path: "home", component: HomeComponent },
+//   { path: "about", component: AboutComponent },
+//   { path: "service", component: ServiceComponent },
+//   { path: "contact", component: ContactsComponent },
+//   {path:"gallery",component:GalleryComponent},
+//   {path: "footer",component:FooterComponent},
+//   { path: "404", component: NotfoundComponent },
+//   // { path: "**", redirectTo: "404" }
+// ];
+
 const routes: Routes = [
-  { path: "", redirectTo: "dashboard", pathMatch: "full" },  // <--- ADD THIS
+  { path: "", redirectTo: "home", pathMatch: "full" }, // <--- Redirect root to 'about'
   { path: "home", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "service", component: ServiceComponent },
   { path: "contact", component: ContactsComponent },
-  {path:"gallery",component:GalleryComponent},
-  {path: "footer",component:FooterComponent},
+  { path: "gallery", component: GalleryComponent },
+  { path: "footer", component: FooterComponent },
   { path: "404", component: NotfoundComponent },
   { path: "**", redirectTo: "404" }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
